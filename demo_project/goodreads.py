e9de1559044fa8155a4110f2d1f1a558149200d8
alimrasheed <m97alim@gmail.com>
2020-01-14 22:12:48 +0300

    moduleNotFound error

---
+++ demo_project/goodreads.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from items import QuoteItem
+
+class GoodReads(scrapy.Spider):
+    name = 'goodreads'
+
+    def requests(self):
+
+        url = 'https://www.goodreads.com/quotes/page=2'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.selector.xpath("//div[@class='quote']"):
+            loader = ItemLoader(item=QuoteItem, selector=quote, response=response)
+            loader.add_xpath('text', ".//div[@class='quoteText']")
+            loader.add_xpath('author', ".//div[@class='quoteText']/child::a")
+            loader.add_xpath('text', ".//div[@class='greyText smallText left']")
+            yield loader.load_item()
+
