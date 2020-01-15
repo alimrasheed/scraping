7f26dd3929fb9f26f4e71a9fa94ca1a9a352e1ad
Alim <alim@localhost.localdomain>
2020-01-15 18:37:33 +0300

    minor changes in items.py

--- demo_project/items.py
+++ demo_project/items.py
@@ -6,12 +6,6 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst, Join
-from w3lib.html import remove_tags
-
-def remove_quotations(value):
-    return value.replace(u"\u201d", '').replace(u"\u201c", '')
-
 
 class QuoteItem(scrapy.Item):
     text= scrapy.Field()
