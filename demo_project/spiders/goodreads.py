be45e5b87c9ec2352dca715a4064ce3a0b97daf3
Alim <alim@localhost.localdomain>
2020-01-20 20:31:04 +0300

    practicing paging

--- demo_project/spiders/goodreads.py
+++ demo_project/spiders/goodreads.py
@@ -23,8 +23,4 @@ class GoodReadsSpider(scrapy.Spider):
             yield loader.load_item()
             
     
-        next_page= response.xpath("//a[@class='next_page']/@href").extract_first()
-
-        if next_page is not None:
-            next_page_link= response.urljoin(next_page)
-            yield scrapy.Request(url=next_page_link, callback=self.parse)
+        
